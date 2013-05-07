7e1c43bacafbd604b86c2f92151f241f87cd46c0
BurnZone <BurnZone@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-05-07 09:10:39 +0000

    updating and tagging version 0.3
    git-svn-id: https://plugins.svn.wordpress.org/burnzone-commenting/tags/0.3@709131 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- conversait.php
+++ conversait.php
@@ -2,11 +2,11 @@
 
 /*
 Plugin Name: Conversait Wordpress Plugin
-Plugin URI: http://theburn-zone.com
+Plugin URI: http://www.theburn-zone.com
 Description: Integrates the Conversait commenting engine
-Version: 0.1.1
+Version: 0.3
 Author: The Burnzone team
-Author URI: http://theburn-zone.com
+Author URI: http://www.theburn-zone.com
 License: GPL2
 */
 
@@ -31,21 +31,34 @@ define('CONVERSAIT_PATH', plugin_dir_path(__FILE__));
 include(CONVERSAIT_PATH . 'options.php');
 include(CONVERSAIT_PATH . 'sso.php');
 
-conv_ensure_options();
+$conv_opt = conv_ensure_options();
 
+/*
+* Function to check whether to replace the default commenting platform or not.
+*/
 function should_replace_comments($post) {
-  global $conv_opt_name_enabled_date;
+  global $conv_opt_name_enabledfor, 
+    $conv_opt_name_activation_type, $conv_opt_name_activation_date, //activation
+    $conv_opt;
+  if (is_null($post))
+    return false;
   $post_time = strtotime($post->post_date);
-  $enabled_time = (int)get_option($conv_opt_name_enabled_date);
-  if ($enabled_time < $post_time)
+  if ($conv_opt[$conv_opt_name_enabledfor][$post->post_type] !== "1")
+    return false;
+  if ($conv_opt[$conv_opt_name_activation_type] === "all")
     return true;
+  if ($conv_opt[$conv_opt_name_activation_type] === "wpcomments_closed")
+    return ($post->comment_status === "closed");
+  if ($conv_opt[$conv_opt_name_activation_type] === "since") {
+    if ($post_time >= $conv_opt[$conv_opt_name_activation_date])
+      return true;
+  }
   return false;
 }
 
 /**
 * Embed the conversait script in the post body.
 */
-
 function conv_comments_template($file) {
   global $post;
   if (should_replace_comments($post))
@@ -53,14 +66,20 @@ function conv_comments_template($file) {
   return $file;
 }
 
-function conv_get_comments_number($count) {
-  global $post;
-  $this_post = NULL;
-  if (func_num_args() > 1)
-    $this_post = get_post(func_get_arg(1));
-  else
-    $this_post = $post;
-  if (should_replace_comments($this_post))
+/**
+* If comment_status == 'closed' then Wordpress does not call 'comments_number'. We can't display the number of comments if 
+* the plugin is enabled for posts with comment_status == 'closed'. This hook overrides comments_open when we're not in admin mode.
+*/
+function conv_comments_open($open, $post_id = null) {
+  $post = get_post($post_id);
+  if (!$open && should_replace_comments($post) && !is_admin())
+    $open = true;
+  return $open;
+}
+
+function conv_get_comments_number($count, $post_id = null) {
+  $post = get_post($post_id);
+  if (should_replace_comments($post))
     return 0;
   return $count;
 }
@@ -74,20 +93,20 @@ function conv_comments_number($output) {
 }
 
 function conv_enqueue_scripts() {
-  wp_enqueue_script('convcommentscount', CONVERSAIT_SERVER_HOST . '/web/javascripts/counts.js');
+  wp_enqueue_script('convcommentscount', CONVERSAIT_SERVER_HOST . '/web/js/counts.js');
 }
 
 function conv_head() {
-  global $conv_opt_name_site_name;
-  $site_name = get_option($conv_opt_name_site_name);
+  global $conv_opt_name_site_name, $conv_opt;
+  $site_name = $conv_opt[$conv_opt_name_site_name];
   echo '<script type="text/javascript">var conversait_sitename = "' . $site_name . '";</script>';
 }
 
-$enabled = get_option($conv_opt_name_enabled);
-$site_name = get_option($conv_opt_name_site_name);
+$site_name = $conv_opt[$conv_opt_name_site_name];
 
-if ($enabled === '1' and isset($site_name) and $site_name !== '') {
+if (isset($site_name) and $site_name !== '') {
   add_filter('comments_template', 'conv_comments_template', 20);
+  add_filter('comments_open', 'conv_comments_open', 20);
   add_filter('get_comments_number', 'conv_get_comments_number', 20);
   add_filter('comments_number', 'conv_comments_number', 20);
   add_action('wp_enqueue_scripts', 'conv_enqueue_scripts');
@@ -95,9 +114,33 @@ if ($enabled === '1' and isset($site_name) and $site_name !== '') {
 }
 
 /**
+* Add dashboard widget
+*/
+add_action( 'wp_dashboard_setup', 'conv_dashboard_widget' );
+function conv_dashboard_widget() {
+    add_meta_box(
+        'conv-dashboard-widget',
+        'Conversait Widget',
+        'conv_dashboard_content',
+        'dashboard',
+        'normal',
+        'high'
+    );
+}
+
+function conv_dashboard_content(){
+  global $conv_opt_name_site_name, $conv_opt;
+    $site_name = $conv_opt[$conv_opt_name_site_name];
+  ?>
+
+  <div class="wrap">
+  <iframe src="<?php echo CONVERSAIT_LOGIN_ROOT . "/signin?redirect=" . urlencode("/admin/moderator?embed=true&site=" . $site_name); ?>" style="width:100%; min-height:500px;"></iframe>
+  </div>
+<?php }
+
+/**
 * Allow redirects to external sites
 */
-
 function conv_allow_redirect($allowed)
 {
   $allowed[] = CONVERSAIT_DOMAIN;
--- uninstall.php
+++ uninstall.php
@@ -1,11 +1,8 @@
 <?php
 
-if(!defined('WP_UNINSTALL_PLUGIN')) exit();
-define('CONVERSAIT_PATH', plugin_dir_path(__FILE__));
-include(CONVERSAIT_PATH . 'options.php');
-delete_option($conv_opt_name_site_name);
-delete_option($conv_opt_name_enabled);
-delete_option($conv_opt_name_enabled_date);
-delete_option($conv_opt_name_sso_logo);
+  if(!defined('WP_UNINSTALL_PLUGIN')) exit();
+  define('CONVERSAIT_PATH', plugin_dir_path(__FILE__));
+  include(CONVERSAIT_PATH . 'options.php');
+  conv_remove_options();
 
 ?>
